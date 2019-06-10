05d07d7a90840e6c1ae2f8babc6b8a487a3fd6e4
hudongyang <hudongyang@xiaohongchun.com>
2019-06-10 10:11:51 +0800

    Initial Commit

---
+++ Clock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clock
+//
+//  Created by hudongyang on 2019/6/10.
+//  Copyright © 2019 hudongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
